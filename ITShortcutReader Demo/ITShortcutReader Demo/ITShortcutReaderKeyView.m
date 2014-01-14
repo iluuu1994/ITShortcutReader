be4d83a31ca83d93574d64a9c4e7f415e47df74e
Ilija Tovilo <ilija.tovilo@me.com>
2014-01-14 10:11:24 +0100

    Switch to black MacBook style keys

--- ITShortcutReader Demo/ITShortcutReader Demo/ITShortcutReaderKeyView.m
+++ ITShortcutReader Demo/ITShortcutReader Demo/ITShortcutReaderKeyView.m
@@ -50,11 +50,11 @@
     [_textField setEditable:NO];
     [_textField setBezeled:NO];
     [_textField setDrawsBackground:NO];
-    [_textField setTextColor:[NSColor colorWithDeviceWhite:0.45f alpha:1.f]];
+    [_textField setTextColor:[NSColor colorWithDeviceWhite:0.9f alpha:1.f]];
     [_textField setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
     
-    _textField.layer.shadowOpacity = 0.6f;
-    _textField.layer.shadowColor = [NSColor colorWithDeviceWhite:1.f alpha:1.f].CGColor;
+    _textField.layer.shadowOpacity = 0.3f;
+    _textField.layer.shadowColor = [NSColor colorWithDeviceWhite:0.f alpha:1.f].CGColor;
     _textField.layer.shadowOffset = (NSSize){ .width = 0.f, .height = 1.f };
     _textField.layer.shadowRadius = 0.f;
     
